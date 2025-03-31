5e0ee3f76f22cf05bc1ec4a927cf8a9103cf09e6
Bob-Maximus <114927549+Bob-Maximus@users.noreply.github.com>
2025-03-31 14:43:18 -0400

    added a goal and a level 2 and the ability to win

--- Lil guy game 2d/Assets/Player/Scripts/PlayerDeath.cs
+++ Lil guy game 2d/Assets/Player/Scripts/PlayerDeath.cs
@@ -7,6 +7,7 @@ public class PlayerDeath : MonoBehaviour
 {
     //what will tell the player they're dead
     public GameObject deathMessage;
+    public GameObject winMessage;
     private void Start()
     {
         //making it invisible to not have it bkock the view
@@ -19,6 +20,12 @@ public class PlayerDeath : MonoBehaviour
         deathMessage.SetActive(true);
         Time.timeScale = 0;
     }
+    public void Win()
+    {
+        //showing the deathmessage and pausing time 
+        winMessage.SetActive(true);
+        Time.timeScale = 0;
+    }
 
     private void OnCollisionEnter2D(Collision2D col)
     {
@@ -26,6 +33,9 @@ public class PlayerDeath : MonoBehaviour
         if (col.gameObject.tag == "bad")
         {
             Death();
+        } else if (col.gameObject.tag == "Win")
+        {
+            Win();
         }
     }
 }
